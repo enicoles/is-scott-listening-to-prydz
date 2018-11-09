6a7cae19b9db94c5f8233e4e9763e4bd8eb5dd6b
Evan Nicoles <evan@solarreviews.com>
2018-11-09 18:30:34 -0500

    Setup CSS

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Is Scott Listening To Prydz?
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
